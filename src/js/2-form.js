c2ab58dc90af4014dc55c5278eff74e92620a726
Artem Lysiak <tiomalysiak@gmail.com>
2024-05-14 17:33:50 +0200

    hw9

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+let formData = {
+  email: '',
+  message: '',
+};
+
+const inputArea = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector('.feedback-form');
+const savedFormData = localStorage.getItem('feedback-form-state');
+
+if (savedFormData) {
+  formData = JSON.parse(savedFormData);
+  document.querySelector('input[name="email"]').value = formData.email;
+  document.querySelector('textarea[name="message"]').value = formData.message;
+}
+
+inputArea.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+submitBtn.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email || formData.message) {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    formData = {
+      email: '',
+      message: '',
+    };
+    document.querySelector('input[name="email"]').value = '';
+    document.querySelector('textarea[name="message"]').value = '';
+  } else {
+    alert('Fill please all fields');
+  }
+});
